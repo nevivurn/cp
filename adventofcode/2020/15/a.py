b838004add5d812bbc16da16e5d55781a71c622b
Yongun Seong <nevivurn@nevi.dev>
2020-12-25 23:15:09 +0900

    adventofcode/2020/15: add

---
+++ adventofcode/2020/15/a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+nums = list(map(int, sys.stdin.readline().rstrip().split(',')))
+seen= dict()
+for i, num in enumerate(nums):
+    seen[num] = i
+
+while len(nums) < 2020:
+    num = nums[-1]
+
+    if num not in seen:
+        nxt = 0
+    else:
+        nxt = len(nums) - seen[num] -1
+
+    seen[nums[-1]] = len(nums)-1
+    nums.append(nxt)
+
+print(nums[-1])
