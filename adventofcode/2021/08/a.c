393038f426cfe77e615cb35a686b75712ac1734e
Yongun Seong <nevivurn@nevi.dev>
2021-12-09 03:08:18 +0900

    adventofcode/2021/08: add

---
+++ adventofcode/2021/08/a.c
@@ -0,0 +1,94 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+unsigned int to_bits(char *str) {
+	unsigned int cur = 0u;
+	while (*str != '\0')
+		cur |= 1<<(*str++ - 'a');
+	return cur;
+}
+
+int to_digit(unsigned int *out, unsigned int v) {
+	for (int i = 0; i < 10; i++)
+		if (out[i] == v)
+			return i;
+	return -1;
+}
+
+void solve(unsigned int *out, unsigned int *sigs) {
+	unsigned int fives[3], sixes[3];
+	size_t five_len = 0, six_len = 0;
+	for (size_t i = 0; i < 10; i++)
+		switch (__builtin_popcount(sigs[i])) {
+		case 2:
+			out[1] = sigs[i];
+			break;
+		case 3:
+			out[7] = sigs[i];
+			break;
+		case 4:
+			out[4] = sigs[i];
+			break;
+		case 7:
+			out[8] = sigs[i];
+			break;
+		case 5:
+			fives[five_len++] = sigs[i];
+			break;
+		case 6:
+			sixes[six_len++] = sigs[i];
+			break;
+		}
+
+	for (size_t i = 0; i < 3; i++)
+		if ((sixes[i] & out[4]) == out[4])
+			out[9] = sixes[i];
+		else if ((sixes[i] & out[1]) == out[1])
+			out[0] = sixes[i];
+		else
+			out[6] = sixes[i];
+
+	for (size_t i = 0; i < 3; i++)
+		if ((fives[i] & out[1]) == out[1])
+			out[3] = fives[i];
+		else if ((fives[i] & out[6]) == fives[i])
+			out[5] = fives[i];
+		else
+			out[2] = fives[i];
+}
+
+int main(void) {
+	char *line = NULL;
+	size_t sz = 0;
+
+	int cnt = 0;
+	while (getline(&line, &sz, stdin) != -1 && line[0] != '\n') {
+		unsigned int sigs[10], solved[10];
+		size_t len = 0;
+
+		char *cur = line, *tok;
+		while ((tok = strtok(cur, " ")) != NULL && tok[0] != '|') {
+			cur = NULL;
+			sigs[len++] = to_bits(tok);
+		}
+
+		solve(solved, sigs);
+
+		while ((tok = strtok(NULL, " \n")) != NULL && tok[0] != '\n') {
+			int digit = to_digit(solved, to_bits(tok));
+			switch (digit) {
+			case 1:
+			case 4:
+			case 7:
+			case 8:
+				cnt++;
+			}
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	free(line);
+}
